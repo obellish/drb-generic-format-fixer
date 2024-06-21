0bc9479fd3e5803e01fcd84a14f0d9e322c780eb
obellish <obellish@mpilabels.com>
2024-06-21 07:46:38 -0400

    fix that

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ use clap::Parser;
 
 #[derive(Debug, Clone, Parser)]
 #[command(version, about, long_about = None)]
+#[repr(transparent)]
 pub struct Args {
 	/// The input folder to search through
 	#[arg(short, long, value_name = "DIRECTORY")]
